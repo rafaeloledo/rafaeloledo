db7cc993e86ff7c6a950551dfec5dcb3a2b026a8
rafaeloledo <rafaeloliveiraledo@gmail.com>
2025-01-15 21:33:16 -0300

    chore: update site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,15 +2,13 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: "Rafael Ledo",
   tagline: "Software Developer",
   url: "https://your-docusaurus-site.example.com",
   baseUrl: "/",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "facebook",
+  projectName: "docusaurus",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -34,11 +32,8 @@ const config: Config = {
             type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
           onUntruncatedBlogPosts: "warn",
@@ -51,39 +46,18 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
+    colorMode: {
+      disableSwitch: true,
+    },
     image: "img/docusaurus-social-card.jpg",
     navbar: {
       title: "Rafael Ledo",
-      items: [
-        // {
-        //   type: "docSidebar",
-        //   sidebarId: "tutorialSidebar",
-        //   position: "left",
-        //   label: "About",
-        // },
-        // { to: "/blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
     },
     footer: {
       style: "dark",
       links: [
         {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        {
-          title: "Community",
+          title: "Social",
           items: [
             {
               label: "Stack Overflow",
@@ -97,23 +71,23 @@ const config: Config = {
               label: "X",
               href: "https://x.com/docusaurus",
             },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
             {
               label: "GitHub",
               href: "https://github.com/facebook/docusaurus",
             },
           ],
         },
+        {
+          title: "Pages",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+          ],
+        },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Rafael Oliveira Ledo`,
     },
     prism: {
       theme: prismThemes.github,
